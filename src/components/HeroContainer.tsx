05a7e693f71e2ffeb2b875c500fe1ddaf0a6e0b7
Musone <yakobkh@gmail.com>
2024-09-17 13:41:48 -0700

    WIP: Hero

--- src/components/HeroContainer.tsx
+++ src/components/HeroContainer.tsx
@@ -6,8 +6,10 @@ interface ContainerProps {
 
 const HeroContainer = ({className} : ContainerProps) => {
   return (
-    <div className={`${className} flex flex-col w-[75%] opacity-[50%] bg-red-300`}>
-        HERO CONTAINER
+    <div className='flex justify-center'>
+      <div className={`${className} flex flex-col w-[75%] opacity-[50%] bg-red-300 h-full`}>
+          HERO CONTAINER
+      </div>
     </div>
   )
 }
